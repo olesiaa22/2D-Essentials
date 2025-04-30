5c2955c6e97a4e44466c6888e342d0e66aeb0008
olesiaa22 <olesiashpotak22@gmail.com>
2025-04-30 12:52:19 +0300

    Add Unity project files

---
+++ Assets/_Unity Essentials/Scripts/Collectible.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+
+
+public class Collectible : MonoBehaviour
+{
+    public float rotationSpeed;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, rotationSpeed, 0);
+    }
+}
